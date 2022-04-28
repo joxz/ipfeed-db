7f4fbf43cd9aacceaae3e3f65a03f3e3ced64f7f
Johannes Denninger <jo@xd.mozmail.com>
2022-04-28 08:40:21 +0200

    fix deletre request, no req body required

--- fn-updentity/index.mjs
+++ fn-updentity/index.mjs
@@ -8,56 +8,56 @@ export default async function (context, req) {
   try {
     let body = '';
 
-    /* check for valid IP or CIDR address in request body */
-    if (
-      isIP(context.bindingData.ip) ||
-      checkCidr.v4(context.bindingData.ip) ||
-      checkCidr.v6(context.bindingData.ip)
-    ) {
-      /* Check if request body has a description */
-      if (
-        context.bindings.req.method === 'PUT' &&
-        typeof context.bindingData.description === 'object'
-      ) {
-        return descriptionNotEmpty();
-      }
+    const entity = {
+      partitionKey: context.bindingData.collection,
+      rowKey: context.bindingData.id,
+      ip: context.bindingData.ip,
+      description: context.bindingData.description,
+    };
 
-      const entity = {
-        partitionKey: context.bindingData.collection,
-        rowKey: context.bindingData.id,
-        ip: context.bindingData.ip,
-        description: context.bindingData.description,
-      };
-
-      /* if request method = PUT, update entity */
-      if (context.bindings.req.method === 'PUT') {
-        try {
-          await updEntity(client, entity);
-          body = {
-            partitionKey: context.bindingData.collection,
-            rowKey: context.bindingData.id,
-            ip: context.bindingData.ip,
-            description: context.bindingData.description,
-          };
-        } catch (err) {
-          return entityNotFound();
+    /* if request method = PUT, update entity */
+    if (context.bindings.req.method === 'PUT') {
+      try {
+        /* check for valid IP or CIDR address in request body */
+        if (
+          isIP(context.bindingData.ip) ||
+          checkCidr.v4(context.bindingData.ip) ||
+          checkCidr.v6(context.bindingData.ip)
+        ) {
+          /* Check if request body has a description */
+          if (
+            context.bindings.req.method === 'PUT' &&
+            typeof context.bindingData.description === 'object'
+          ) {
+            return descriptionNotEmpty();
+          }
+        } else {
+          return noValidIP();
         }
-      }
 
-      /* if request method = DELETE, delete entity */
-      if (context.bindings.req.method === 'DELETE') {
-        try {
-          await rmEntity(client, entity.partitionKey, entity.rowKey);
-          body = {
-            partitionKey: context.bindingData.collection,
-            rowKey: context.bindingData.id,
-          };
-        } catch (err) {
-          return entityNotFound();
-        }
+        await updEntity(client, entity);
+        body = {
+          partitionKey: context.bindingData.collection,
+          rowKey: context.bindingData.id,
+          ip: context.bindingData.ip,
+          description: context.bindingData.description,
+        };
+      } catch (err) {
+        return entityNotFound();
       }
-    } else {
-      return noValidIP();
+    }
+
+    /* if request method = DELETE, delete entity */
+    if (context.bindings.req.method === 'DELETE') {
+      try {
+        await rmEntity(client, entity.partitionKey, entity.rowKey);
+        body = {
+          partitionKey: context.bindingData.collection,
+          rowKey: context.bindingData.id,
+        };
+      } catch (err) {
+        return entityNotFound();
+      }
     }
 
     return sendSuccess(body);
